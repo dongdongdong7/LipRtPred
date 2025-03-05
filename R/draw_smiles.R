84afb6851602631d0eb1a1813663c2cb51a56db8
spd <384220729@qq.com>
2025-03-05 10:01:05 +0800

    Update new function draw_smi.

--- R/draw_smiles.R
+++ R/draw_smiles.R
@@ -24,3 +24,39 @@ draw_smiles <- function(smiles, SMARTS = NULL, molsPerRow = 1L, subImgSize = c(4
   file.remove(tempfile_name)
   invisible(NULL)
 }
+
+#' @title Draw a molecule using a SMILES
+#'
+#' @param smi A SMILES string
+#' @param width Width of pic
+#' @param height Height of pic
+#' @param addAtomIndices Whether to add atom indices
+#' @param addBondIndices Whether to add bond indices
+#' @param addStereoAnnotation Whether to add stereoisomorphic annotations
+#' @param explicitMethyl Whether to display the terminal methyl group
+#'
+#' @return NULL
+#' @export
+#'
+#' @examples
+#' draw_smi(smi = "C(OCCCCCCCC(CC)(CCC)CCCCC(C)C)C(OC(CCCCCCCCCCCC(C)C)=O)COC(=O)CCCCCCC(OCCC)CCCCC(C)C",
+#'          width = 1600, height = 800)
+draw_smi <- function(smi, width = 450, height = 150,
+                     addAtomIndices = TRUE,
+                     addBondIndices = FALSE,
+                     addStereoAnnotation = FALSE,
+                     explicitMethyl = FALSE){
+  reticulate::source_python(system.file("python", "draw_smis.py", package = "LipRtPred"))
+  tempfile_name <- paste0(tempfile(), ".png")
+  draw_smi_py(smi = smi,
+              filePath = tempfile_name,
+              width = as.integer(width), height = as.integer(height),
+              addAtomIndices = addAtomIndices,
+              addBondIndices = addBondIndices,
+              addStereoAnnotation = addStereoAnnotation,
+              explicitMethyl = explicitMethyl)
+  img <- png::readPNG(tempfile_name)
+  grid::grid.raster(img)
+  file.remove(tempfile_name)
+  invisible(NULL)
+}
--- inst/python/draw_smis.py
+++ inst/python/draw_smis.py
@@ -1,5 +1,6 @@
 from rdkit.Chem import Draw
 from rdkit import Chem
+from rdkit.Chem.Draw import rdMolDraw2D
 
 def flatten_tuple(nested):
   flat_list = []
@@ -21,3 +22,15 @@ def draw_smis(smis, filePath, SMARTS = None, molsPerRow = 1, subImgSize = (400, 
     mols.append(mol)
   img = Draw.MolsToGridImage(mols, highlightAtomLists = highlights, molsPerRow = molsPerRow, subImgSize = subImgSize)
   img.save(filePath)
+
+def draw_smi_py(smi, filePath, width, height, addAtomIndices = True, addBondIndices=False, addStereoAnnotation = True, explicitMethyl=False):
+  mol = Chem.MolFromSmiles(smi)
+  d = rdMolDraw2D.MolDraw2DCairo(width = width, height = height)
+  do = d.drawOptions()
+  do.addAtomIndices = addAtomIndices
+  do.addBondIndices = addBondIndices
+  do.addStereoAnnotation = addStereoAnnotation
+  do.explicitMethyl = explicitMethyl
+  d.DrawMolecule(mol)
+  d.FinishDrawing()
+  d.WriteDrawingText(filePath)
