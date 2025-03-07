5d26d873a51e56706424becec992fceba575df83
spd <384220729@qq.com>
2025-03-07 16:59:38 +0800

    Remove BreakRings function

--- inst/python/molecule_operation.py
+++ inst/python/molecule_operation.py
@@ -46,85 +46,65 @@ def GetAtomSymbol_py(smi, atom_idx_list):
     symbolList.append(atom.GetSymbol())
   return symbolList
 
+'''
 # Break all the rings in the molecule
 def BreakRings_py(smi):
   mol = Chem.MolFromSmiles(smi)
+  Chem.Kekulize(mol)
   if mol is None:
     raise ValueError("Wrong SMILES")
-  
+
   rw_mol = Chem.RWMol(mol)
-  
+
   ring_info = rw_mol.GetRingInfo()
   rings = ring_info.AtomRings()
   rings = tuple(sorted(rings, key = len))
-  
+
   for ring in rings:
-    single_bonds_CC = []
-    single_bonds_Other = []
-    double_bonds_CC = []
-    double_bonds_Other = []
-    triple_bonds_CC = []
-    triple_bonds_Other = []
-    
+    bonds_CC = []
+    bonds_Other = []
+
     for i in range(len(ring)):
       for j in range(i + 1, len(ring)):
         bond = rw_mol.GetBondBetweenAtoms(ring[i],ring[j])
         if bond is not None:
-          bond_type = bond.GetBondType()
+          # bond_type = bond.GetBondType()
           atom1 = bond.GetBeginAtom().GetSymbol()
           atom2 = bond.GetEndAtom().GetSymbol()
           
-          if bond_type == Chem.BondType.SINGLE:
-            if atom1 == "C" and atom2 == "C":
-              single_bonds_CC.append(bond)
-            else:
-              single_bonds_Other.append(bond)
-          elif bond_type == Chem.BondType.DOUBLE:
-            if atom1 == "C" and atom2 == "C":
-              double_bonds_CC.append(bond)
-            else:
-              double_bonds_Other.append(bond)
-          elif bond_type == Chem.BondType.TRIPLE:
-            if atom1 == "C" and atom2 == "C":
-              triple_bonds_CC.append(bond)
-            else:
-              triple_bonds_Other.append(bond)
-      
-    if single_bonds_CC:
-      bond_to_break = single_bonds_CC[0]
-    elif single_bonds_Other:
-      bond_to_break = single_bonds_Other[0]
-    elif double_bonds_CC:
-      bond_to_break = double_bonds_CC[0]
-    elif double_bonds_Other:
-      bond_to_break = double_bonds_Other[0]
-    elif triple_bonds_CC:
-      bond_to_break = triple_bonds_CC[0]
-    elif triple_bonds_Other:
-      bond_to_break = triple_bonds_Other[0]
+          if atom1 == "C" and atom2 == "C":
+            bonds_CC.append(bond)
+          else:
+            bonds_Other.append(bond)
+    
+    if bonds_CC:
+      bond_to_break = bonds_CC[0]
+    elif bonds_Other:
+      bond_to_break = bond_Other[0]
     else:
       continue
-    
+
     rw_mol.RemoveBond(bond_to_break.GetBeginAtom().GetIdx(), bond_to_break.GetEndAtom().GetIdx())
-    
-    mol = rw_mol.GetMol()
-    for atom in mol.GetAtoms():
-      atom.SetAtomMapNum(atom.GetIdx())
-    
-    return(Chem.MolToSmiles(mol))
+
+  mol = rw_mol.GetMol()
+  for atom in mol.GetAtoms():
+    atom.SetAtomMapNum(atom.GetIdx())
+
+  return(Chem.MolToSmiles(mol, kekuleSmiles = True))
+'''
 
 # Traverse the entire molecule starting from the start atom, excluding the specified atoms
 def TraverseMolecule_py(smi, start_atom_idx, non_traversable_atom_idx):
   mol = Chem.MolFromSmiles(smi)
   if mol is None:
     raise ValueError("Wrong SMILES")
-  
+
   if start_atom_idx < 0 or start_atom_idx >= mol.GetNumAtoms():
     raise ValueError("Wrong start_atom_idx")
-  
+
   visited_initial = set(non_traversable_atom_ids)
   path = []
-  
+
   def dfs(atom_idx, visited):
     if atom_idx in visited:
       return
@@ -133,6 +113,6 @@ def TraverseMolecule_py(smi, start_atom_idx, non_traversable_atom_idx):
     atom = mol.GetAtomWithIdx(atom_idx)
     for neighbor in atom.GetNeighbors():
       dfs(atom_idx = neighbor.GetIdx(), visited = visited)
-  
+
   dfs(start_atom_idx, visited = visited_initial)
   return(path)
